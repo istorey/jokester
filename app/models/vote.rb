e80af51a7e19dbc8fd9c3258bd38492a231e346d
JackCurtis <jack.v.curtis@gmail.com>
2015-01-09 12:16:12 -0500

    validates presence of vote type

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,7 +1,7 @@
 class Vote < ActiveRecord::Base
 	belongs_to :joke
 	belongs_to :user
-	validates :type, inclusion: { in:[ "up", "down"] }
+	validates :type, inclusion: { in:[ "up", "down"] }, presence: true
 	validates :joke_id, numericality: true, presence: true
 	validates :user_id, numericality: true, presence: true
 end
