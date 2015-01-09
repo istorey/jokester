c3f5af52be1e33a596707c5ceb1e8750952b4766
istorey <wrightstoreys@gmail.com>
2015-01-09 10:58:32 -0500

    tested Joke model, body

---
+++ app/models/joke.rb
@@ -0,0 +1,3 @@
+class Joke < ActiveRecord::Base
+	validates :body, presence: true
+end
---
+++ spec/models/joke_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+require_relative "../../app/models/joke.rb"
+
+describe Joke do
+	describe "::new" do
+		it "can be instantiated" do
+			j = Joke.new
+			expect(j.class).to eq(Joke)
+		end
+	end
+
+	it { should validate_presence_of(:body) }
+	it { should validate_presence_of(:user_id)}
+end
